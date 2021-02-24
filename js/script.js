44df59c66b45fa5be776c4354c9808f594d53937
Andrea Vidal <11.andrea.v.p@gmail.com>
2021-02-24 16:50:22 +0100

    dias ingreso y media edad de mujeres

--- js/script.js
+++ js/script.js
@@ -1,86 +1,77 @@
 const pacientes = [
 	{
 		paciente: {
-			nombre: 'Manuel',
+			nombre: "Manuel",
 			edad: 25,
-			sexo: 'H'
+			sexo: "H"
 		},
 		diasIngresado: 3,
-		dieta: 'Baja en fibra'
+		dieta: "Baja en fibra"
 	},
 	{
 		paciente: {
-			nombre: 'Marta',
+			nombre: "Marta",
 			edad: 56,
-			sexo: 'M'
+			sexo: "M"
 		},
 		diasIngresado: 5,
-		dieta: 'Diabetes'
+		dieta: "Diabetes"
 	},
 	{
 		paciente: {
-			nombre: 'Julia',
+			nombre: "Julia",
 			edad: 38,
-			sexo: 'M'
+			sexo: "M"
 		},
 		diasIngresado: 1,
-		dieta: 'Sin sal'
+		dieta: "Sin sal"
 	},
 	{
 		paciente: {
-			nombre: 'Esteban',
+			nombre: "Esteban",
 			edad: 40,
-			sexo: 'H'
+			sexo: "H"
 		},
 		diasIngresado: 3,
-		dieta: 'Diabetes'
+		dieta: "Diabetes"
 	},
 	{
 		paciente: {
-			nombre: 'Arturo',
+			nombre: "Arturo",
 			edad: 19,
-			sexo: 'H'
+			sexo: "H"
 		},
 		diasIngresado: 2,
-		dieta: 'Baja en fibra'
+		dieta: "Baja en fibra"
 	},
 	{
 		paciente: {
-			nombre: 'Isabel',
+			nombre: "Isabel",
 			edad: 63,
-			sexo: 'M'
+			sexo: "M"
 		},
 		diasIngresado: 6,
-		dieta: 'Sin sal'
+		dieta: "Sin sal"
 	}
 ];
 
 datosHospital(pacientes);
 
 function datosHospital(lista) {
-	const reducer = (accumulator, currentValue) => accumulator + currentValue;
+	const reducer = (acc, currentValue) => acc + currentValue;
+
 	const edad = lista.filter(lista => lista.paciente.edad >= 18);
 	const dieta = lista.filter(lista => lista.paciente.sexo === "H" && lista.dieta === "Diabetes");
-	const totalDiasIngreso = lista.diasIngresado.reduce(reducer);
-	const mediaEdadMujeres = 1;
+	const totalDiasIngreso = lista.map(lista => lista.diasIngresado).reduce(reducer);
+	const mediaEdadMujeres = (lista.filter(lista => lista.paciente.sexo === "M").map(lista => lista.paciente.edad).reduce(reducer) / lista.filter(lista => lista.paciente.sexo === "M").length);
 
+	console.log(mediaEdadMujeres);
 
-	console.log(totalDiasIngreso);
-
-	const resultados = {
+	return resultados = {
 		nPacientes: lista.length,
 		nMayoresEdad: edad.length,
 		nHombresDiabeticos: dieta.length,
-		totalDiasIngreso: "",
+		totalDiasIngreso,
 		mediaEdadMujeres: ""
-	}
-
-
+	};
 }
-
-
-/* nPacientes: (número de pacientes),
-nMayoresEdad: (número de pacientes mayores de edad),
-nHombresDiabeticos: (número de pacientes hombres con dieta para diabetes),
-totalDiasIngreso: (suma de los días de ingreso de todos los pacientes),
-mediaEdadMujeres: (edad media de las pacientes mujeres) */
